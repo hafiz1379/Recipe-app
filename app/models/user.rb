9a0ccf2aeea57df27c7567e3a7f96efb9945a45c
hafiz1379 <hafizrasa1379@gmail.com>
2023-11-13 14:23:38 +0430

    Add confirmable module to User model and create resend confirmation instructions view.

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable
+  has_many :foods
+end
