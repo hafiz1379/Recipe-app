1259abbdebe86dc1d72a5fb11074ca1d72234be2
hafiz1379 <hafizrasa1379@gmail.com>
2023-11-15 17:00:58 +0430

    Add dependent destroy to User-Food association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
-  has_many :foods
+
+  has_many :foods, dependent: :destroy
 end
