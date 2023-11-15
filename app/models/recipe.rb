969e07169edebf051532383e67ac23b8973be63a
StarMindz <64619873+StarMindz@users.noreply.github.com>
2023-11-15 23:33:25 +0100

    Implement merge conflict correction

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,16 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :foods, join_table: 'recipe_foods'
+
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :preparation_time, :cooking_time, numericality: { greater_than_or_equal_to: 0 }, allow_blank: true
+  validates :public, inclusion: { in: [true, false] }
+
+  before_validation :set_default_public
+
+  private
+
+  def set_default_public
+    self.public = true if public.nil?
+  end
 end
--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,16 +1,4 @@
-class Recipe < ApplicationRecord
-  has_many :recipe_foods
-  has_many :foods, through: :recipe_foods
-
-  validates :name, presence: true, length: { maximum: 250 }
-  validates :preparation_time, :cooking_time, numericality: { greater_than_or_equal_to: 0 }, allow_blank: true
-  validates :public, inclusion: { in: [true, false] }
-
-  before_validation :set_default_public
-
-  private
-
-  def set_default_public
-    self.public = true if public.nil?
-  end
+class RecipeFood < ApplicationRecord
+  belongs_to :recipe, class_name: 'Recipe'
+  belongs_to :food, class_name: 'Food'
 end
