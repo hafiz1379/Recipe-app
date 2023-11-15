b15129584a039b07161be5d84e03cbf04b3ce1db
hafiz1379 <hafizrasa1379@gmail.com>
2023-11-15 17:01:25 +0430

    Add route for "foods" page

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
   # Defines the root path route ("/")
   root "home#index"
+
+   # Defines the route for the "foods" page ("/foods")
+   resources :foods, only: [:index, :new, :create, :destroy]
 end
