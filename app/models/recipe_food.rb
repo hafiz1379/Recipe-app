201342bc09adeffdeefc57671db309839ad50cac
StarMindz <64619873+StarMindz@users.noreply.github.com>
2023-11-16 11:07:19 +0100

    correct linters

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -2,7 +2,7 @@ class RecipeFood < ApplicationRecord
   belongs_to :recipe, class_name: 'Recipe'
   belongs_to :food, class_name: 'Food'
 
-  
+
   # Avoids duplicated ingredients.
   validates :food_id, uniqueness: true
 end
