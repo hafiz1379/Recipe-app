b33c6e7798fc14309b48e7690396e25dba54ef98
hafiz1379 <hafizrasa1379@gmail.com>
2023-11-16 15:30:59 +0430

    Fix indentation and syntax errors in CSS and Ruby files

--- app/helpers/foods_helper.rb
+++ app/helpers/foods_helper.rb
@@ -1,10 +1,10 @@
 module FoodsHelper
-    def get_measurement_prefix(measurement)
-      options = {
-        'grams' => 'g',
-        'milliliters' => 'ml',
-        'units' => 'units'
-      }
-      options[measurement]
-    end
+  def get_measurement_prefix(measurement)
+    options = {
+      'grams' => 'g',
+      'milliliters' => 'ml',
+      'units' => 'units'
+    }
+    options[measurement]
   end
+end
